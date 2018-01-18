0abf704ea754d35677abe3b3449c3613a49655dd
accrete-pramodp <accrete.pramodp@gmail.com>
2018-01-18 16:57:26 +0530

    Add files via upload

--- short_url_submit.php
+++ short_url_submit.php
@@ -1,6 +1,6 @@
 <?php 
 include("functions.php"); //include functions.php where all the functions are defined.
-$url = redirect($_POST["url"]); //call the function create()
+$url = redirect($_POST["url"]);
 
 echo json_encode($url);
 ?>
--- url_submit.php
+++ url_submit.php
@@ -1,6 +1,6 @@
 <?php 
 include("functions.php"); //include functions.php where all the functions are defined.
-$url = create($_POST["url"]); //call the function create()
+$url = create($_POST["url"]); 
 
  echo json_encode($url);
 ?>
